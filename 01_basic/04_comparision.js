cea8fefb9d42a62b17c056e790e284c410d32c91
Vijay Satish Patil <patilviju0106@gmail.com>
2025-08-24 19:19:25 +0530

    04-comparsion.js added

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2<3)
+// console.log(5>9)
+// console.log(4>=4)
+// console.log(2!=5)
+
+// console.log("2">1);            //true
+// console.log("02">1);           //true
+
+// console.log(null>0);           //false
+// console.log(null==0);          //false
+// console.log(null>=0);          //true
+// console.log(null<=0);          //true
+
+// console.log(undefined==0);     //false
+// console.log(undefined>=0);     //false
+// console.log(undefined<=0);     //false
+
+// console.log("2"==2);            //true
+// console.log("2"===2);           //false
+
+
+//      '==' doesnot check datatype while comparision 
+//      '==='  check datatype while comparision 
