b5e2f81419dff9b31c10b6ae80be9f25bc02fe90
Vijay Satish Patil <patilviju0106@gmail.com>
2025-08-24 19:52:57 +0530

    updated all file

--- 01_basic/02_datatype.js
+++ 01_basic/02_datatype.js
@@ -1,8 +1,8 @@
-let name="vijay";
-let age=23;
-let isLogin=true;
+// let name="vijay";
+// let age=23;
+// let isLogin=true;
 
-console.log([name,age,isLogin]);
+// console.log([name,age,isLogin]);
 
 //   Datatype
 // number => 2^53
@@ -15,10 +15,67 @@ console.log([name,age,isLogin]);
 // object
 
 // to check datatype
-console.log(typeof(name)) //string
-console.log(typeof(age))// number
-console.log(typeof(isLogin)) // boolean
-console.log(typeof(null))// object
-console.log(typeof(undefine))// undefined
-console.log(typeof(Symbol))// function
+// console.log(typeof(name)) //string
+// console.log(typeof(age))// number
+// console.log(typeof(isLogin)) // boolean
+// console.log(typeof(null))// object
+// console.log(typeof(undefine))// undefined
+// console.log(typeof(Symbol))// function
+
+// 2 type Datatype
+
+// Primative Datatype :
+//     String, Number , BogInt , Boolean , Null , undefined ,Symbol  (no float datatype , float value comes under Number datatype)
+
+// Non-Primtive datatype :
+//     Array , Object , Function
+
+
+// let tump =null;
+// if (tump>0){
+//     console.log("high");
+
+// }
+// else if  (tump<0) {
+//     console.log("low");
+// }else if (tump=0){
+//     console.log("0");
+// }
+// else{
+//     console.log("null");
+    
+// }
+
+    
+// const num1=Symbol("abc");
+// const num2=Symbol("abc");
+// console.log(num1);        // Symbol(abc)
+// console.log(num2);        // Symbol(abc)
+// console.log(num1==num2);  //false
+
+
+// Array
+// const heros=['salman','srk','ajay devgan','vijay thalapati'];
+// console.log(heros);         //[ 'salman', 'srk', 'ajay devgan', 'vijay thalapati' ]
+
+
+
+// Object
+// const obj={
+//     hero:"Vijay Thalapati",
+//     heroin:"Anuska sharma",
+//     cost:50000
+// };
+// console.log(obj);           //{ hero: 'Vijay Thalapati', heroin: 'Anuska sharma', cost: 50000 }
+
+
+
+// function
+// let fun=function(){
+//     console.log("This is Function");
+// }
+// console.log(fun());         //This is Function
+
+
+
 
--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,3 +1,5 @@
+// *********************Conversion************************************************
+
 // =====================Number===========================================
 
 // let score="99"
@@ -16,11 +18,11 @@
 
 // =====================String===========================================
 
-let val = false
-console.log(typeof val);    //boolean
-let str = String(val)
-console.log(str);            // false
-console.log(typeof str);     // string
+// let val = false
+// console.log(typeof val);    //boolean
+// let str = String(val)
+// console.log(str);            // false
+// console.log(typeof str);     // string
 
 
 
@@ -42,4 +44,39 @@ console.log(typeof str);     // string
 // "33abc" => True
 // " " => false
 // null => False
+
+
+// *********************Operation************************************************
+
+// let val=5;
+// let negval=-val;
+// console.log(negval);
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2-2);
+// console.log(2%2);
+// console.log(2/2);
+// console.log(2**2);
+
+// let str1='vijay';
+// let str2="patil";
+// let str3=str1+" "+str2;   
+// console.log(str3);      // vijay patil
+// console.log(1+"2");     // 12
+// console.log("1"+2);     // 12
+// console.log("1"+"2");   // 12
+// console.log(1+1+'2');   // 22
+// console.log("1"+2+"3"); // 123
+
+
+let score=100;
+console.log(score); //100
+score++
+console.log(score); //101
+score+1
+console.log(score); //101
+score+=1
+console.log(score); //102
+++score
+console.log(score); //103
 
