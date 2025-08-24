cbd21ec4ec4a9638f3da0c3eb56d7f7f63f3e160
Vijay Satish Patil <patilviju0106@gmail.com>
2025-08-24 16:32:13 +0530

    03_conversionOperation.js added

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,45 @@
+// =====================Number===========================================
+
+// let score="99"
+// console.log(typeof score);
+// let num=Number(score)
+// console.log(typeof num);
+
+
+// any datatype to Number
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+// null => 0
+
+
+// =====================String===========================================
+
+let val = false
+console.log(typeof val);    //boolean
+let str = String(val)
+console.log(str);            // false
+console.log(typeof str);     // string
+
+
+
+
+
+// =====================Boolean===========================================
+
+// let logIn =undefined
+// let bool = Boolean(logIn)
+// console.log(typeof logIn);    //number
+// console.log(bool);            // false
+// console.log(typeof bool);     // boolean
+
+
+// any datatype to boolean
+// 1 => true
+// 0 => false
+// "33" => True
+// "33abc" => True
+// " " => false
+// null => False
+
