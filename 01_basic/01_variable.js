721db379d568158d6ed8366fe786ecf7a567be99
Vijay Satish Patil <patilviju0106@gmail.com>
2025-08-24 16:03:11 +0530

    Intial commite

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,7 @@
+const userId=162003;
+let name='raj';
+var email="vijay@gmail.com"
+city="Pune"
+console.log(name);
+
+console.table([userId,name,email,city])
---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,24 @@
+let name="vijay";
+let age=23;
+let isLogin=true;
+
+console.log([name,age,isLogin]);
+
+//   Datatype
+// number => 2^53
+// bigint
+// string => " "  , ' '
+// boolean => true, false
+// null
+// undefine
+// symbol => unique
+// object
+
+// to check datatype
+console.log(typeof(name)) //string
+console.log(typeof(age))// number
+console.log(typeof(isLogin)) // boolean
+console.log(typeof(null))// object
+console.log(typeof(undefine))// undefined
+console.log(typeof(Symbol))// function
+
